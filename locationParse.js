6dbb9b255e97da33b0795c214671a7fb8be08c4d
NinjaNic <omg.my.junkmail@gmail.com>
2018-01-21 05:25:04 -0800

    fix this damn country thing

--- locationParse.js
+++ locationParse.js
@@ -4,28 +4,38 @@ var tx = document.getElementById('txt');
 
 function post(url) {
     xmlhttp.open("GET", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
     xmlhttp.send();
 }
 
 function checkCountry(location) {
+    post('https://battuta.medunes.net/api/country/code/us/?key=87930e6715aba8281eef8e907fdd8dfb'); return;
     // key = 87930e6715aba8281eef8e907fdd8dfb
-    var url = 'http://battuta.medunes.net/api/country/code/us/?key=87930e6715aba8281eef8e907fdd8dfb';
-    var csc = [];
-    csc = location.split(',');
+    var url = 'http://battuta.medunes.net/api/country/search/?';
+    var csc = location.split(',');
     for (var i = 0; i < csc.length; i++) {
-        csc.trim();
+        csc[i] = csc[i].trim();
     }
+    console.log(csc.length);
     switch (csc.length) {
         case 0:
-
+            // error
+            break;
         case 1:
-
+            // city
+            url += 'city=' + csc[0]; break;
         case 2:
-
+            // city, state
+            console.log(csc[0] + csc[1]);
+            url += 'city=' + csc[0] + '&region=' + csc[1]; break;
         case 3:
-
+        // city, state, country
+        url += 'city=' + csc[0] + '&region=' + csc[1] + '&country=' + csc[2]; break;
         default:
     }
+    url += '&key=87930e6715aba8281eef8e907fdd8dfb';
+    console.log(url);
     post(url);
     // post('https://restcountries.eu/rest/v2/name/' + location);
 }
